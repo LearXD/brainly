13f52530025b0d0b6716ace6b4570ff90da418e4
LearXD <gutyxdcanal@gmail.com>
2023-10-06 17:59:43 -0300

    Exported Modules

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
 import Brainly from "./utils/brainly";
-
-(async () => {
-  const instance = new Brainly();
-  const response = await instance.search('Quem foi o primeiro presidente do Brasil?');
-  const question = await instance.getQuestionById(response.results[0].question.id) //
-
-  console.log(question.data.questionById.answers)
-})()
-
+export { Brainly }
